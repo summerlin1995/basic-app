4ebd179cc8f1e232ed903a301dd88205b2894d66
summerlin <summer.lin@logicsolutions.com>
2019-12-13 15:48:14 +0800

    make the two tabs on header switchable

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() featureSelected = new EventEmitter();
 
+    onSelect(feature: 'string'){
+        this.featureSelected.emit(feature);
+    }
 }
