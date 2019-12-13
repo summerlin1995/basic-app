85bc181269342f39d1cc8ec461294fb778e79f5e
summerlin <summer.lin@logicsolutions.com>
2019-12-13 11:02:52 +0800

    the final submit

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
+import { Ingredient } from '../shared/ingredient.module';
 
 @Component({
   selector: 'app-shopping-list',
